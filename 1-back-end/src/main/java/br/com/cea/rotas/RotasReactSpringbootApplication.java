d0a52b26adc07d38fdda97cc64886b21bd2e18d3
Henrique Lins <henriquelins@msn.com>
2021-01-17 13:55:20 -0300

    Project created

---
+++ 1-back-end/src/main/java/br/com/cea/rotas/RotasReactSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.cea.rotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotasReactSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotasReactSpringbootApplication.class, args);
+	}
+
+}
---
+++ 1-back-end/src/test/java/br/com/cea/rotas/RotasReactSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cea.rotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotasReactSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
